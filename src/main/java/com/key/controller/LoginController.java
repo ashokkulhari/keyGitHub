cc7eac52b0fc657f5b050409166911bcb987dc5f
ashokkulhari <ashok.kulhari@gmail.com>
2018-10-22 06:45:50 +0530

    update crossorigin

--- src/main/java/com/key/controller/LoginController.java
+++ src/main/java/com/key/controller/LoginController.java
@@ -24,6 +24,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 public class LoginController {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
--- src/main/java/com/key/controller/ProductController.java
+++ src/main/java/com/key/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.support.ManagedMap;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import com.key.util.CommonUtils;
 
 
 @RestController
+@CrossOrigin
 public class ProductController {
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 	
