c023301853dff144d050120186032f0f59bb9aea
ashokkulhari <ashok.kulhari@gmail.com>
2018-10-30 15:30:45 +0530

    update

--- src/main/java/com/key/configuration/WebMVCSupport.java
+++ src/main/java/com/key/configuration/WebMVCSupport.java
@@ -2,6 +2,7 @@ package com.key.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,4 +16,9 @@ public class WebMVCSupport implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry) {
       registry.addInterceptor(verifyAccessInterceptor).addPathPatterns("/**");
   }
+  
+  @Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
 }
