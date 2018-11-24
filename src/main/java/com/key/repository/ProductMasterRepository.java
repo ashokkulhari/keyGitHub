1a9330a8b0efa49671e805a537a6d0eae9da1fac
ashokkulhari <ashok.kulhari@gmail.com>
2018-11-24 21:44:43 +0530

    update active only product

--- src/main/java/com/key/repository/ProductMasterRepository.java
+++ src/main/java/com/key/repository/ProductMasterRepository.java
@@ -11,26 +11,28 @@ import com.key.model.ProductMaster;
 
 public interface ProductMasterRepository extends JpaRepository<ProductMaster, Integer>{
 
-	
+	@Query("SELECT e FROM ProductMaster e WHERE e.isActive=1 and e.itemCode =:itemCode order by itemName ASC ")
 	List<ProductMaster> findByItemCode(String itemCode);
+	
+	@Query("SELECT e FROM ProductMaster e WHERE e.isActive=1 and e.itemName =:itemName order by itemName ASC ")
 	List<ProductMaster> findByItemName(String itemName);
 	
 //	@Query("SELECT e FROM ProductMaster e WHERE  e.itemCode =:itemCode order by itemName ASC ")
 //	List<ProductMaster> findByItemCode(@Param("itemCode") String itemCode);
 	
-	@Query("SELECT e FROM ProductMaster e WHERE e.itemName like %:itemName%  order by itemName ASC ")
+	@Query("SELECT e FROM ProductMaster e WHERE e.isActive=1 and e.itemName like %:itemName%  order by itemName ASC ")
 	List<ProductMaster> matchByItemName(@Param("itemName") String itemName);
 	
-	@Query("SELECT e FROM ProductMaster e WHERE e.groupCode.groupCodeId =:groupCodeId order by itemName ASC ")
+	@Query("SELECT e FROM ProductMaster e WHERE e.isActive=1 and e.groupCode.groupCodeId =:groupCodeId order by itemName ASC ")
 	List<ProductMaster> findByGroupCode(@Param("groupCodeId") Integer groupCodeId);
 	
-	@Query("SELECT e FROM ProductMaster e WHERE e.category.categoryId =:categoryId order by itemName ASC ")
+	@Query("SELECT e FROM ProductMaster e WHERE e.isActive=1 and e.category.categoryId =:categoryId order by itemName ASC ")
 	List<ProductMaster> findByCategory(@Param("categoryId") Integer categoryId);
 	
-	@Query("SELECT e FROM ProductMaster e WHERE e.manufacture.manufactureId =:manufactureId order by itemName ASC ")
+	@Query("SELECT e FROM ProductMaster e WHERE e.isActive=1 and e.manufacture.manufactureId =:manufactureId order by itemName ASC ")
 	List<ProductMaster> findByManufacture(@Param("manufactureId") Integer manufactureId);
 	
 	
-	@Query("SELECT e FROM ProductMaster e WHERE e.vendor.vendorId =:vendorId order by itemName ASC ")
+	@Query("SELECT e FROM ProductMaster e WHERE e.isActive=1 and e.vendor.vendorId =:vendorId order by itemName ASC ")
 	List<ProductMaster> findByVendor(@Param("vendorId") Integer vendorId);
 }
